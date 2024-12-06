581f9457ac82087504c1da05683cb3f5473a5636
calculus0129 <ryanbhang129@naver.com>
2024-12-07 07:30:46 +0900

    fix: npm start works!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react'; // Can omit in modern react projects
+// import React from 'react'; // Can omit in modern react projects
+import Todos from './components/Todos';
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
     <div>
-      
+      <Todos />
     </div>
   );
 }
