7fbbf237a74a118dca7cde87b64d503f4aa13060
calculus0129 <ryanbhang129@naver.com>
2024-12-07 13:20:02 +0900

    feat: Add the feature for new Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 // import React from 'react'; // Can omit in modern react projects
 import Todos from './components/Todos';
 import NewTodo from './components/NewTodo';
-// import logo from './logo.svg';
 // import './App.css';
 import Todo from './models/todo';
+import { useState } from 'react';
 
 function App() {
 
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript'),
-  ];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // const todos = [
+  //   new Todo('Learn React'),
+  //   new Todo('Learn TypeScript'),
+  // ];
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
   return (
     <div>
-      <NewTodo />
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos}/>
     </div>
   );
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+// import React from "react";
+import { useRef } from "react";
 
-const NewTodo: React.FC = (props) => {
+const NewTodo: React.FC<{onAddTodo: (arg0: string) => void}> = (props) => {
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
+    
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
+
+        // !: The todoTextInputRef is NEVER 'null'.
+        const enteredText = todoTextInputRef.current!.value;
+
+
+        if (enteredText.trim().length === 0) {
+            // throw an error
+            return;
+        }
+
+        props.onAddTodo(enteredText);
     };
     return (
         <form onSubmit={submitHandler}>
             <label htmlFor='text'>Todo text</label>
-            <input type='text' id='text' />
+            <input type='text' id='text' ref={todoTextInputRef} />
             <button> Add Todo</button>
         </form>
     );
